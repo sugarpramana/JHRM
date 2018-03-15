5f368e9f5879522142633a9fd451fe33fa114be4
tekosulaiman <tekosulaiman@yahoo.com>
2018-03-15 17:20:27 +0700

    Holiday

--- Module-Parent/Module-Run/src/main/java/org/app/portofolio/webui/hr/transaction/leave/parameter/holiday/HolidayVM.java
+++ Module-Parent/Module-Run/src/main/java/org/app/portofolio/webui/hr/transaction/leave/parameter/holiday/HolidayVM.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 import net.sf.jasperreports.engine.JRException;
 
-import org.app.portofolio.webui.hr.master.job.jobtitle.model.MstJobtitleListItemRenderer;
-import org.module.hr.model.MstJobtitle;
-import org.module.hr.service.JobService;
+import org.app.portofolio.webui.hr.transaction.leave.parameter.holiday.model.MstHolidayListitemRenderer;
+import org.module.hr.model.MstHoliday;
+import org.module.hr.service.LeaveService;
 import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.Command;
@@ -30,6 +30,7 @@ import org.zkoss.zul.Paging;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.event.PagingEvent;
 
+@SuppressWarnings({ "rawtypes", "unchecked" })
 public class HolidayVM {
 	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	 * Wire component
@@ -40,19 +41,19 @@ public class HolidayVM {
 	@Wire("#listboxHoliday")
 	private Listbox listboxHoliday;
 	
-	@Wire("#pagingJobTitle")
-	private Paging pagingJobTitle;
+	@Wire("#pagingHoliday")
+	private Paging pagingHoliday;
 
 	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	 * Service yang dibutuhkan sesuai bisnis proses
 	 *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-	private MstJobtitle mstJobtitle;
-	private List<MstJobtitle> mstJobtitles;
+	private MstHoliday mstHoliday;
+	private List<MstHoliday> mstHolidays;
 	@WireVariable
-	private JobService jobService;
-	private ListitemRenderer<MstJobtitle> listitemRenderer;
+	private LeaveService leaveService;
+	private ListitemRenderer<MstHoliday> listitemRenderer;
 	
-	private HashMap<String, Object> hashMapJobTitle;
+	private HashMap<String, Object> hashMapMstHoliday;
 	
 	private int startPageNumber = 0;
 	private int pageSize = 10;
@@ -66,26 +67,26 @@ public class HolidayVM {
 		listboxHoliday.setStyle("border-style: none;");
 		listboxHoliday.setMold("paging");
 		
-		int count = jobService.getCountMstJobtitles();
+		int count = leaveService.getCountMstHolidays();
 
-		pagingJobTitle.setTotalSize(count);
-		pagingJobTitle.setDetailed(true);
-		pagingJobTitle.setPageSize(pageSize);
+		pagingHoliday.setTotalSize(count);
+		pagingHoliday.setDetailed(true);
+		pagingHoliday.setPageSize(pageSize);
 	}
 	
 	private void refreshPageList(int refreshActivePage) {
 		if (refreshActivePage == 0) {
-			pagingJobTitle.setActivePage(0);
+			pagingHoliday.setActivePage(0);
 		}
 		
 		refreshActivePage += 1;
 		
-		hashMapJobTitle = new HashMap<String, Object>();
-		hashMapJobTitle.put("firstResult", refreshActivePage);
-		hashMapJobTitle.put("maxResults", pagingJobTitle.getPageSize());
+		hashMapMstHoliday = new HashMap<String, Object>();
+		hashMapMstHoliday.put("firstResult", refreshActivePage);
+		hashMapMstHoliday.put("maxResults", pagingHoliday.getPageSize());
 		
-		mstJobtitles = jobService.getMstJobtitlePaging(hashMapJobTitle);
-		listitemRenderer = new MstJobtitleListItemRenderer();
+		mstHolidays = leaveService.getMstHolidayPaging(hashMapMstHoliday);
+		listitemRenderer = new MstHolidayListitemRenderer();
 	}
 
 	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -94,7 +95,7 @@ public class HolidayVM {
 	@AfterCompose
 	public void setupComponents(@ContextParam(ContextType.VIEW) Component component, 
 		@ExecutionArgParam("object") Object object,
-		@ExecutionArgParam("mstJobtitle") MstJobtitle mstJobtitle) {
+		@ExecutionArgParam("mstHoliday") MstHoliday mstHoliday) {
 		
 		Selectors.wireComponents(component, this, false);
 
@@ -106,9 +107,9 @@ public class HolidayVM {
 	 * Function CRUD Event
 	 *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 	@Command
-	@NotifyChange("mstJobtitles")
+	@NotifyChange("mstHolidays")
 	public void doFilter(){
-		mstJobtitles.clear();
+		mstHolidays.clear();
         
 		String getName = textboxFilter.getValue();
 		
@@ -117,14 +118,14 @@ public class HolidayVM {
 			refreshPageList(startPageNumber);
 		} else {
 			HashMap<String, Object> hashMap = new HashMap<String, Object>();
-			hashMap.put("jobName", getName);
-			mstJobtitles = jobService.getByMstJobtitleRequestMap(hashMap);
-			listitemRenderer = new MstJobtitleListItemRenderer();
+			hashMap.put("holidayName", getName);
+			mstHolidays = leaveService.getByMstHolidayRequestMap(hashMap);
+			listitemRenderer = new MstHolidayListitemRenderer();
 		}
 	}
 	
 	@Command
-	@NotifyChange("mstJobtitles")
+	@NotifyChange("mstHolidays")
 	public void onPaging(@ContextParam(ContextType.TRIGGER_EVENT) PagingEvent pagingEvent){
 		startPageNumber = pagingEvent.getActivePage() * pageSize;
 		refreshPageList(startPageNumber);
@@ -132,13 +133,13 @@ public class HolidayVM {
 
 	@Command
 	public void doNew(){
-		final ListModelList<MstJobtitle> listModelListJobtitle = (ListModelList) listboxHoliday.getModel();
-		listModelListJobtitle.add(0, new MstJobtitle());
+		final ListModelList<MstHoliday> listModelListMstHoliday = (ListModelList) listboxHoliday.getModel();
+		listModelListMstHoliday.add(0, new MstHoliday());
 	}
 	
 	@Command
 	public void doDelete(){
-		final ListModelList<MstJobtitle> listModelListJobtitle = (ListModelList) listboxHoliday.getModel();
+		final ListModelList<MstHoliday> listModelListMstHoliday = (ListModelList) listboxHoliday.getModel();
 		
 		if(listboxHoliday.getSelectedIndex() == -1){
 			Messagebox.show("There is no selected record?", "Confirm", Messagebox.OK, Messagebox.ERROR);
@@ -146,9 +147,9 @@ public class HolidayVM {
 			Messagebox.show("Do you really want to remove item?", "Confirm", Messagebox.OK | Messagebox.CANCEL, Messagebox.EXCLAMATION, new EventListener() {
 			    public void onEvent(Event event) throws Exception {    	
 			 		if (((Integer) event.getData()).intValue() == Messagebox.OK) {
-			 			for(MstJobtitle jobtitle: listModelListJobtitle){
-			 				if(listModelListJobtitle.isSelected(jobtitle)){
-			 					jobService.delete(jobtitle);
+			 			for(MstHoliday mstHoliday: listModelListMstHoliday){
+			 				if(listModelListMstHoliday.isSelected(mstHoliday)){
+			 					leaveService.delete(mstHoliday);
 			 				}
 			 			}
 			 			
@@ -162,7 +163,7 @@ public class HolidayVM {
 	}
 	
 	@Command
-	@NotifyChange("mstJobtitles")
+	@NotifyChange("mstHolidays")
 	public void doRefresh(){
 		doPrepareList();
 		refreshPageList(startPageNumber);
@@ -174,12 +175,44 @@ public class HolidayVM {
 	}
 	
 	@GlobalCommand
-	@NotifyChange("mstJobtitles")
+	@NotifyChange("mstHolidays")
 	public void refreshAfterSaveOrUpdate(){
-		mstJobtitles = jobService.getAllMstJobtitles();
+		doPrepareList();
+		refreshPageList(startPageNumber);
 	}
-
+	
 	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	 * Getter Setter
 	 *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+	public MstHoliday getMstHoliday() {
+		return mstHoliday;
+	}
+
+	public void setMstHoliday(MstHoliday mstHoliday) {
+		this.mstHoliday = mstHoliday;
+	}
+
+	public List<MstHoliday> getMstHolidays() {
+		return mstHolidays;
+	}
+
+	public void setMstHolidays(List<MstHoliday> mstHolidays) {
+		this.mstHolidays = mstHolidays;
+	}
+
+	public LeaveService getLeaveService() {
+		return leaveService;
+	}
+
+	public void setLeaveService(LeaveService leaveService) {
+		this.leaveService = leaveService;
+	}
+
+	public ListitemRenderer<MstHoliday> getListitemRenderer() {
+		return listitemRenderer;
+	}
+
+	public void setListitemRenderer(ListitemRenderer<MstHoliday> listitemRenderer) {
+		this.listitemRenderer = listitemRenderer;
+	}
 }
