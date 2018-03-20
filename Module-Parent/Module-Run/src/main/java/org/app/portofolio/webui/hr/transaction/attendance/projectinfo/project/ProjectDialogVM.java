e8b2c9c1af418cdf0535003f3f2c57d5e6386c5e
tekosulaiman <tekosulaiman@yahoo.com>
2018-03-20 14:15:31 +0700

    Add Dialog Project

--- Module-Parent/Module-Run/src/main/java/org/app/portofolio/webui/hr/transaction/attendance/projectinfo/project/ProjectDialogVM.java
+++ Module-Parent/Module-Run/src/main/java/org/app/portofolio/webui/hr/transaction/attendance/projectinfo/project/ProjectDialogVM.java
@@ -1,18 +1,13 @@
 package org.app.portofolio.webui.hr.transaction.attendance.projectinfo.project;
 
-import org.module.hr.model.MstProject;
 import org.zkoss.bind.annotation.AfterCompose;
-import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.Selectors;
 
-@SuppressWarnings({ "rawtypes", "unchecked" })
-public class ProjectListVM {
-
+public class ProjectDialogVM {
 	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	 * Wire component
 	 *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
@@ -24,14 +19,13 @@ public class ProjectListVM {
 	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	 * Function Custom sesuai kebutuhan
 	 *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-
+	
 	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	 * Inisialize Methode MVVM yang pertama kali dijalankan
 	 *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 	@AfterCompose
-	public void setupComponents(@ContextParam(ContextType.VIEW) Component component, 
-		@ExecutionArgParam("object") Object object,
-		@ExecutionArgParam("mstProject") MstProject mstProject) {
+	public void setupComponents(@ContextParam(ContextType.VIEW) Component component,
+		@ExecutionArgParam("object") Object object) {
 		
 		Selectors.wireComponents(component, this, false);
 	}
@@ -39,10 +33,6 @@ public class ProjectListVM {
 	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	 * Function CRUD Event
 	 *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-	@Command
-	public void doNew(){
-		Executions.createComponents("/WEB-INF/pages/module_hr/transaction/attendance/project_info/project/projectDialog.zul", null, null);
-	}
 	
 	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	 * Getter Setter
